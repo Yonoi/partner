c0e3d532e7b4762f7282b0c063eb6d1d8567518a
pipixiapi <38618697+pipixiapi@users.noreply.github.com>
2022-02-18 10:57:07 +0800

    Add files via upload

--- jimmy/10.py
+++ jimmy/10.py
@@ -3,22 +3,37 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-#https://www.nowcoder.com/practice/435fb86331474282a3499955f0a41e8b?tpId=13&tqId=23294&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
-# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
 #
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#https://www.nowcoder.com/practice/91b69814117f4e8097390d107d2efbe0?tpId=13&tqId=23454&ru=/practice/91b69814117f4e8097390d107d2efbe0&qru=/ta/coding-interviews/question-ranking
 # 
 # @param pRoot TreeNode类 
-# @return int整型
+# @return int整型二维数组
 #
 class Solution:
-    def TreeDepth(self , pRoot: TreeNode) -> int:
+    def Print(self , pRoot: TreeNode) -> List[List[int]]:
         # write code here
-        if pRoot == None:
-            return 0
-        left = right = 0
-        if pRoot.left:
-            left = self.TreeDepth(pRoot.left)
-        if pRoot.right:
-            right = self.TreeDepth(pRoot.right)
-        return max(left, right)+1
+        ture_to_false = True
+        queue = []
+        result = []
+        queue.append(pRoot)
+        while queue:
+            num = len(queue)
+            tmp = []
+            for i in range(num):
+                node = queue.pop(0)
+                if not node:
+                    continue
+                queue.append(node.left)
+                queue.append(node.right)
+                tmp.append(node.val)
+            if tmp:
+                if ture_to_false:
+                    result.append(tmp)
+                else:
+                    tmp.reverse()
+                    result.append(tmp)
+            ture_to_false = not ture_to_false
+        return result
+            
             
