e8d7ead0b362a4aa493e59eb2caa7dc776d907af
Yonoi <15909202870@163.com>
2022-02-25 16:14:18 +0800

    2. Add Two Numbers

---
+++ msun/2.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry, reminder = (l1.val + l2.val) // 10, (l1.val + l2.val) % 10
+        newListNode = ListNode(reminder)
+        tmpListNode = newListNode
+        while l1.next != None and l2.next != None:
+            l1, l2 = l1.next, l2.next
+            carry, reminder = (l1.val + l2.val + carry) // 10, (l1.val + l2.val + carry) % 10
+            tmpListNode.next = ListNode(reminder)
+            tmpListNode = tmpListNode.next
+        
+        res_list = l2 if l1.next == None else l1
+        while res_list.next != None:
+            res_list = res_list.next
+            carry, reminder = (res_list.val + carry) // 10, (res_list.val + carry) % 10
+            tmpListNode.next = ListNode(reminder)
+            tmpListNode = tmpListNode.next
+            
+        if carry != 0:
+            tmpListNode.next = ListNode(carry)
+        return newListNode
+            
