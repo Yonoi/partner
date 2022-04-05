6088ff0528c489397e313580f7dccabe895b92ea
Yonoi <15909202870@163.com>
2022-04-05 17:22:51 +0800

    add some files

---
+++ msun/dfs/98.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+        self.pre_val = float('-inf')
+    def isValidBST(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        else:
+            if not self.isValidBST(root.left):
+                return False
+            if root.val > self.pre_val:
+                self.pre_val = root.val
+            else:
+                return False
+            return self.isValidBST(root.right)
+        
