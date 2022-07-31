1ae32531a3a9679e477e443b95ad06cb4083addb
Yonoi <15909202870@163.com>
2022-07-31 12:29:01 +0800

    300. Longest Increasing Subsequence

--- msun/dp/300.py
+++ msun/dp/300.py
@@ -1,14 +1,12 @@
-# 300. Longest Increasing Subsequence
-from typing import List
+"300. Longest Increasing Subsequence"
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = []
-        dp.append(1)
-        
-        for i in range(1, n):
-            try:
-                dp.append(max([each for idx, each in enumerate(dp) if nums[idx] < nums[i]]) + 1)
-            except ValueError:
-                dp.append(1)
-        return max(dp)
+        dp = [1] * n
+        ans = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            ans = max(ans, dp[i])
+        return ans
